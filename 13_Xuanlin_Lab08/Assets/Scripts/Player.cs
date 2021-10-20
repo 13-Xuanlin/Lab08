c6d398c53c2431a78c2574fbffd167ff653b1b1e
13-Xuanlin <xuanlin56@hotmail.com>
2021-10-20 12:00:54 +0800

    Updated Player and Spawner Script

---
+++ 13_Xuanlin_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Text scoreText;
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+
+
+        if (transform.position.y >= 5.5)
+        {
+
+            transform.position = new Vector3(transform.position.x, 5.5f, transform.position.z);
+        }
+        if (transform.position.y <= -3)
+        {
+
+            transform.position = new Vector3(transform.position.x, -3f, transform.position.z);
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+
+        }
+        if (collision.gameObject.tag == "Point")
+        {
+            score++;
+            scoreText.text = "SCORE: " + score;
+            /* audioSource.PlayOneShot(Point);
+             CheckScore();*/
+        }
+    }
+}
---
+++ 13_Xuanlin_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    public GameObject SpawnObject2;
+    public GameObject SpawnObject3;
+    private int hi;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        int i = Random.Range(1, 4);
+        if (i == 1)
+        {
+            Instantiate(SpawnObject, transform.position, transform.rotation);
+        }
+        if (i == 2)
+        {
+            Instantiate(SpawnObject2, transform.position, transform.rotation);
+        }
+        if (i == 3)
+        {
+            Instantiate(SpawnObject3, transform.position, transform.rotation);
+        }
+    }
+   
+}
